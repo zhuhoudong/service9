2709dcb9eb669ad8d78566207635a910b5c6895d
paas <paas@cse-codegen-backend-963378786-b13t3>
2017-12-01 06:54:29 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/service9/controller/Service9Delegate.java
@@ -0,0 +1,14 @@
+package com.service.service9.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Service9Delegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
